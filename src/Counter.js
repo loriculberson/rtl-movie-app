3e85f38ae2df2c9922cc305150315f9dae9b0319
Lori Culberson <lori_culberson@homedepot.com>
2020-02-13 09:53:40 -0500

    Add Counter tests

--- src/Counter.js
+++ src/Counter.js
@@ -2,9 +2,13 @@ import React, { useState} from 'react';
 
 const Counter = () => {
   const [count, setCount] = useState(0)
+
+  const increment = () => {
+    setCount(count + 1)
+  }
   return (
     <div>
-      <button>{count}</button>
+      <button onClick={increment} data-testid='counter-button'>{count}</button>
     </div>
   );
 };
--- src/Counter.test.js
+++ src/Counter.test.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import { render, cleanup } from 'react-testing-library'
-import Counter from './Counter'
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import Counter from "./Counter";
 
-describe('Counter', () => {
-  it('renders on the page', () => {
-    const wrapper = render(<Counter/>)
+describe("Counter", () => {
+  it("renders on the page", () => {
+    const { debug, getByTestId } = render(<Counter />);
+    const counterButton = getByTestId("counter-button")
+    
+    expect(counterButton.tagName).toBe('BUTTON')
+    expect(counterButton.textContent).toBe("0");
+  });
+
+  it('increments the count value', () => {
+    const { debug, getByTestId } = render(<Counter />);
+    const counterButton = getByTestId("counter-button")
+
+    fireEvent.click(counterButton)
+    expect(counterButton.textContent).toBe("1");
+
+    fireEvent.click(counterButton)
+    expect(counterButton.textContent).toBe("2");
   })
-})
+});
